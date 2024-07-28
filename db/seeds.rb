4d6e0a85fb22715a54e7e5870694ddf53349679d
Janaka Vithanage <jkvithana@gmail.com>
2024-07-28 18:23:25 +1000

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,35 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'json'
+
+verticals_file = File.read('./json/verticals.json')
+categories_file = File.read('./json/categories.json')
+courses_file = File.read('./json/courses.json')
+
+verticals_data = JSON.parse(verticals_file)
+categories_data = JSON.parse(categories_file)
+courses_data = JSON.parse(courses_file)
+
+# seed vertical data
+verticals_data.each do |vertical_data|
+  vertical = Vertical.find_or_initialize_by(id: vertical_data['Id'])
+  vertical.name = vertical_data['Name']
+  p vertical if vertical.save!
+end
+
+# seed category data
+categories_data.each do |category_data|
+  category = Category.find_or_initialize_by(id: category_data['Id'])
+  category.name = category_data['Name']
+  category.state = category_data['State']
+  category.vertical_id = category_data['Verticals']
+  p category if category.save!
+end
+
+# seed course data
+courses_data.each do |course_data|
+  course = Course.find_or_initialize_by(id: course_data['Id'])
+  course.name = course_data['Name']
+  course.author = course_data['Author']
+  course.state = course_data['State']
+  course.category_id = course_data['Categories']
+  p course if course.save!
+end
